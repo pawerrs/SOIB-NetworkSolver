b5479a8d2880802af258786497c913b091311351
Paweł Kowalik <01112962@pw.edu.pl>
2020-04-23 20:32:41 +0200

    initial commit

---
+++ NetworkSolver.Models/Link.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    public class Link
+    {
+        public int LinkId { get; set; }
+        public int[] ConnectedByLink { get; set; }
+        public List<int> LambdasIds { get; set; }
+
+        public Link(int linkId, int firstNodeId, int secondNodeId)
+        {
+            this.LinkId = linkId;
+            ConnectedByLink = new int[2];
+            ConnectedByLink[0] = firstNodeId;
+            ConnectedByLink[1] = secondNodeId;
+            LambdasIds = new List<int>();
+        } 
+
+        public Link Clone()
+        {
+            return new Link(this.LinkId, this.ConnectedByLink[0], this.ConnectedByLink[1]);
+        }
+    }
+}
