2fa0a5720df7957767c0b47422a406df71a2f891
Paweł Kowalik <01112962@pw.edu.pl>
2020-04-23 21:18:47 +0200

    added runner file

--- NetworkSolver/Program.cs
+++ NetworkSolver/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using NetworkSolver.Common;
+using NetworkSolver.Common.Genetic;
+using NetworkSolver.Common.Topology;
+using NetworkSolver.DataAccess.InputFileParser;
+using NetworkSolver.Services.GeneticService;
+using System;
 
 namespace NetworkSolver
 {
@@ -6,7 +11,45 @@ namespace NetworkSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Halko tu SOIBY");
+            InputFileParser parser = new InputFileParser();
+            Console.WriteLine("Choose network to optimize:");
+            Console.WriteLine("1: Network_1.txt");
+            Console.WriteLine("2: Network_2.txt");
+            string choice = Console.ReadLine();
+            Network inputNetwork = new Network();
+            if (choice.Equals("1") || choice.Equals("2"))
+            {
+                inputNetwork = parser.ReadNetwork("Network_" + choice + ".txt");
+            }
+            else
+            {
+                return;
+            }
+
+            //Find all node pairs
+            PathFinder pathFinder = new PathFinder();
+
+            for (int i = 1; i < inputNetwork.Nodes.Count; i++)
+            {
+                for (int j = i + 1; j < inputNetwork.Nodes.Count + 1; j++)
+                {
+                    pathFinder.FindAllPaths(i, j, inputNetwork);
+                }
+            }
+
+            // all paths generated. Finding perfect combination.
+            GeneticAlgorithmParameters parameters = new GeneticAlgorithmParameters();
+            parameters.InitialPopulationSize = 100;
+            parameters.CrossoverProbability = (float)0.2;
+            parameters.MutationProbability = (float)0.1;
+            parameters.RandomSeed = 4253;
+            parameters.LimitValue = 30;
+            parameters.StoppingCriteria = StoppingCriteria.NoImprovement;
+
+            string fileName = "Network_" + choice;
+            GeneticService geneticService = new GeneticService(parameters, inputNetwork, pathFinder, fileName);
+            geneticService.Solve();
+
         }
     }
 }
